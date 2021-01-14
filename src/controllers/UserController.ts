7f7db1f2d7385a187d7b6aa35018362db027d7bd
Cassio Cappellari <cassiocappellari@gmail.com>
2021-01-14 08:15:36 -0300

    controller de usuario criado; model de usuario criado

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from 'express'
+import {getRepository} from 'typeorm'
+import User from '../models/User'
+
+
+export default {
+    async create(req: Request, res: Response) {
+        const {
+            name,
+            email,
+            password
+        } = req.body
+
+        const userRepository = getRepository(User)
+
+        const data = {
+            name,
+            email,
+            password
+        }
+
+        try {
+            const user = userRepository.create(data)
+            await userRepository.save(user)
+        } catch(error) {
+            res.status(400).send({message: 'error creating user'})
+        }
+    }
+}
