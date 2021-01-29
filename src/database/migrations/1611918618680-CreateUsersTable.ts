252ce51ff81bbcd60dae500d0087e8dd390970f1
Cassio Cappellari <cassiocappellari@gmail.com>
2021-01-29 09:20:52 -0300

    refactor: configuracoes do typeorm atualizadas indicando o local dos arquivos de migrations; migrations adicionadas ao projeto; migration da tabela users adicionada

---
+++ src/database/migrations/1611918618680-CreateUsersTable.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm"
+
+export class CreateUsersTable1611918618680 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+            {
+                name: 'id',
+                type: 'integer',
+                unsigned: true,
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment'
+            },
+            {
+                name: 'name',
+                type: 'varchar'
+            },
+            {
+                name: 'email',
+                type: 'varchar',
+                isUnique: true
+            },
+            {
+                name: 'password',
+                type: 'varchar'
+            }
+        ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+}
